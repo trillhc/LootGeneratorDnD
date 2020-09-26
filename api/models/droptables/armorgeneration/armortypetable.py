91c919a73af3ed8abc002244d9a0a7b602afeb9e
Thomas Ebens <bemusedknave@gmail.com>
2020-09-26 00:40:53 -0400

    Added more Tables, remaining tables tomorrow

--- api/models/droptables/armorgeneration/armortypetable.py
+++ api/models/droptables/armorgeneration/armortypetable.py
@@ -4,11 +4,11 @@ from .base import db
 
 class ArmorTypeGeneration(Mixin, db.Model):
     __tablename__ = "armortypetable"
-# This table determines type of armor (padded, leather, breastplate, fullplate, etc) 
+# This table determines type of armor, reference RandomArmorType (Table 3) in google sheets
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    encounterLevel = db.Column(db.Integer, nullable=False)
-    minPercentage = db.Column(db.Integer, nullable=False)
-    maxPercentage = db.Column(db.Integer, nullable=False)
-    result = db.Column(db.String, nullable=False)
+    minPercentage = db.Column(db.Integer)
+    maxPercentage = db.Column(db.Integer)
+    result = db.Column(db.String) #armorTypeName in sheets
+    armorCost = db.Column(db.Integer)
     
--- api/models/droptables/armorgeneration/shieldtypetable.py
+++ api/models/droptables/armorgeneration/shieldtypetable.py
@@ -4,7 +4,8 @@ from .base import db
 
 class ShieldTypeGeneration(Mixin, db.Model):
     __tablename__ = "shieldtypetable"
-# This table generates type of shield
+# This table generates type of shield, reference randomshieldtype (Table 4) in google sheets
+# https://docs.google.com/spreadsheets/d/19AtJAPoJwQ5SkND8xWPy-sUWymMjrH6zaNybFoD0iQs/edit#gid=0
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
     encounterLevel = db.Column(db.Integer, nullable=False)
