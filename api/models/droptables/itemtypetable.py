d13b11839164e1099f351565a21047e4fe7cc798
Thomas Ebens <bemusedknave@gmail.com>
2020-09-27 22:21:30 -0400

    removed evil excess tabless

---
+++ api/models/droptables/itemtypetable.py
@@ -0,0 +1,15 @@
+from api.core import Mixin
+from .base import db
+
+
+class ItemTypeTable(Mixin, db.Model):
+    __tablename__ = "itemtype"
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    typeArmor = db.Column(db.Integer)
+    typeShield = db.Column(db.Integer)
+    typeMeleeWeapon = db.Column(db.Integer)
+    typeRangeWeapon = db.Column(db.Integer)
+    minPercentage = db.Column(db.Integer)
+    maxPercentage = db.Column(db.Integer)
+    result = db.Column(db.String)
+    
---
+++ api/models/droptables/magicitem.py
@@ -0,0 +1,17 @@
+from api.core import Mixin
+from .base import db
+
+
+class MagicItemTable(Mixin, db.Model):
+    __tablename__ = "magicitemtable"
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    minPercentage = db.Column(db.Integer)
+    maxPercentage = db.Column(db.Integer)
+    enchantmentPotency = db.Column(db.Integer)
+    specificPiece = db.Column(db.Integer)
+    meleeOrRanged = db.Column(db.Integer)
+    enchantmentValue = db.Column(db.Integer)
+    enchantmentCost = db.Column(db.Integer)
+    result = db.Column(db.String) 
+    weaponCost = db.Column(db.Integer)
