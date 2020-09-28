417fe69a8f85884b14aea77bb038466e9cf9d7e0
Thomas Ebens <bemusedknave@gmail.com>
2020-09-27 22:51:42 -0400

    more comments for clarity

--- api/models/droptables/itemtypetable.py
+++ api/models/droptables/itemtypetable.py
@@ -6,10 +6,7 @@ class ItemTypeTable(Mixin, db.Model):
     # This table is used to determine types (armor/shield and melee/ranged)
     __tablename__ = "itemtype"
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    typeArmor = db.Column(db.Integer)
-    typeShield = db.Column(db.Integer)
-    typeMeleeWeapon = db.Column(db.Integer)
-    typeRangeWeapon = db.Column(db.Integer)
+    typeItem = db.Column(db.String)
     minPercentage = db.Column(db.Integer)
     maxPercentage = db.Column(db.Integer)
     result = db.Column(db.String)
--- api/models/spellList.py
+++ api/models/spellList.py
@@ -7,14 +7,14 @@ class spellList(Mixin, db.Model):
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
     spellName = db.Column(db.String, nullable=False)
-    primarySchool = db.Column(db.String)
+    primarySchool = db.Column(db.String)  #A spell can have multiple schools and sub-schools
     priSubSchool = db.Column(db.Integer)
     secondarySchool = db.Column(db.Integer)
     secSubSchool = db.Column(db.Integer)
     tertiarySchool = db.Column(db.String)
     tertSubSchool = db.Column(db.String)
     bardSpellLevel = db.Column(db.Integer)
-    clericSpellLevel = db.Column(db.Integer)
+    clericSpellLevel = db.Column(db.Integer) # different classes have different levels for same spell
     domainSpellName = db.Column(db.String)
     domainSpellLevel = db.Column(db.String)
     druidSpellLevel = db.Column(db.Integer)
@@ -22,7 +22,7 @@ class spellList(Mixin, db.Model):
     rangerSpellLevel = db.Column(db.Integer)
     sorcererSpellLevel = db.Column(db.Integer)
     wizardSpellLevel = db.Column(db.Integer)
-    verbalSpellComponent = db.Column(db.String)
+    verbalSpellComponent = db.Column(db.String) # Components for the spell in question
     somaticSpellComponent = db.Column(db.String)
     materialSpellComponent = db.Column(db.String)
     focusSpellComponent = db.Column(db.String)
