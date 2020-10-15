79f4dc1e7cf6d0a48fa1a9291901a42632f14e19
trillhc <trillhc@gmail.com>
2020-10-15 17:55:12 -0400

    mundane

--- api/models/droptables/mundaneItems.py
+++ api/models/droptables/mundaneItems.py
@@ -6,14 +6,15 @@ class MundaneItem(Mixin, db.Model):
     __tablename__ = "mundaneitem"
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    itemName = db.Column(db.String, nullable=False)
+    itemName = db.Column(db.String)
     #itemClass is "init" for initial roll then "armor" or "tools" ect.
     itemClass = db.Column(db.String)
+    supercategory = db.Column(db.Boolean)
     itemCost = db.Column(db.Integer)
     itemCostToCreate = db.Column(db.Integer)
     minPercentage = db.Column(db.Integer, nullable=False)
     maxPercentage = db.Column(db.Integer, nullable=False)
-    result = db.Column(db.String, nullable=False)
+    result = db.Column(db.String)
 
 
     def __init__(self, id: str):
